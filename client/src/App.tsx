5f20dd7fa9819e30fbb3526fff531ea093baf85d
Nithish Kumar N <kit27.am34@gmail.com>
2026-09-02 09:27:54 +0530

    Configure GH Pages deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Home as HomeIcon, User, ShoppingBag, PieChart } from 'lucide-react';
 import './index.css';
 
